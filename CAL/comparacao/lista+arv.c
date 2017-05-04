3e2879379cf03b349bf355988914807704d1fa1b
Unknown <jonathan@simplificca.com>
2017-05-04 07:11:43 -0300

    Relatório Comparativo
    +Lista Encadeada - (File IO, Time, Single Linked)
    +ArvoreBin - (FIle IO, Time, ArvBin)
    +Hash(-File IO,-Time,ArvBin,Lista)

---
+++ CAL/comparacao/lista+arv.c
@@ -0,0 +1,123 @@
+#include "lista+arv.h"
+//----------------
+//Funções p/ Hash
+//----------------
+int stringToInt(char nome[100],char sobrenome[100]){
+	int a=0,b=0,c=0;
+	a = strlen(nome);
+	a += strlen(sobrenome);
+	b = nome[0];
+	c = sobrenome[0];
+	return ((a*b)-(c-a))*37;
+}
+
+int geraHash(int prehash, int tamanhotabela){
+	return prehash % tamanhotabela;
+}
+
+//----------------
+//Funções Arvore
+//----------------
+Nodo *iniciaArvore(){
+    return NULL;
+}
+
+void insere(int valor, char nome[100],char sobrenome[100], Nodo **raiz)
+{
+    if( *raiz == NULL )
+    {
+        *raiz = (Nodo*) malloc( sizeof( Nodo ) );
+        (*raiz)->number = valor;
+        strcpy((*raiz)->nome,nome);
+        strcpy((*raiz)->sobrenome,sobrenome);
+        (*raiz)->esq = NULL;
+        (*raiz)->dir = NULL;
+    }else {
+          if (valor < (*raiz)->number){
+            insere(valor,nome,sobrenome,&(*raiz)->esq);
+        }
+        if (valor > (*raiz)->number){
+            insere(valor,nome,sobrenome,&(*raiz)->dir);
+        }
+    }
+}
+
+void mostra(Nodo *raiz)
+{
+   if (raiz != NULL){
+        mostra(raiz->esq);
+        printf ("\n %i\n", raiz->number);
+        mostra(raiz->dir);
+    }
+}
+
+unsigned int busca(char nome[100],char sobrenome[100], Nodo *raiz){
+    if((strcmp(raiz->nome,nome)==0)&&(strcmp(raiz->sobrenome,sobrenome))==0){
+     return raiz->number;
+    }
+    while (raiz != NULL){
+      if(raiz->dir == NULL){
+        return busca(nome,sobrenome,raiz->esq);
+      }
+      else{
+        return busca(nome,sobrenome,raiz->dir);
+      }
+    }
+  return ERRO;
+}
+
+//----------------
+//Funções Lista
+//----------------
+void inicializaLista(tLista *lista){
+    lista->n_elem = 0;
+    lista->cabecalho = (tNo *) malloc(sizeof(tNo));
+    }
+
+int listaVazia(tLista lista){
+    if(lista.n_elem == 0){
+        return SIM;
+    }
+    else {
+        return NAO;
+    }
+}
+
+void inserirNoInicio(tLista *lista, int number, char nome[100],char sobrenome[100]){
+    tNo *no;
+    no = (tNo *) malloc(sizeof(tNo));
+    no->number = number;
+    strcpy(no->nome, nome);
+    strcpy(no->sobrenome, sobrenome);
+    if(lista->n_elem == 0){
+    no->prox = NULL;
+    lista->cabecalho = no;
+    lista->n_elem = lista->n_elem +1 ;
+    }
+    else{
+        no->prox = lista->cabecalho;
+        lista->cabecalho = no;
+        lista->n_elem = lista->n_elem +1 ;
+    }
+}
+
+void mostraLista(tLista lista){
+    int i;
+    printf("\nDados da lista:\n");
+    for(i=0;i<lista.n_elem;i++){
+    printf("\tId: %i\tNome: %s\tSobrenome: %s\n",lista.cabecalho->number,lista.cabecalho->nome,lista.cabecalho->sobrenome);
+    lista.cabecalho = lista.cabecalho->prox;
+    }
+}
+
+
+unsigned int buscarNomeSobrenome(tLista lista, char nome[100],char sobrenome[100]){
+  unsigned int i;
+  for(i=0;i<lista.n_elem;i++){
+    if((strcmp(lista.cabecalho->nome,nome)==0)&&(strcmp(lista.cabecalho->sobrenome,sobrenome))==0){
+      return lista.cabecalho->number;
+    }
+    lista.cabecalho = lista.cabecalho->prox;
+  }
+  return ERRO;
+}
---
+++ CAL/comparacao/lista+arv.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIM 1
+#define NAO 0
+#define ERRO 987654321
+//----------------
+//Estrutura Arvore
+//----------------
+typedef struct No{
+   struct No *dir;
+   struct No *esq;
+   unsigned int number;
+   char nome[100];
+   char sobrenome[100];
+}Nodo;
+
+void insere(int valor, char nome[100],char sobrenome[100], Nodo **raiz);
+void mostra(Nodo *raiz); //infixada
+Nodo * removerNodo(int valor,Nodo *raiz);
+Nodo* FindMin(Nodo* raiz);
+int deleteNodo(int dado, Nodo* raiz);
+int removeNodo(Nodo *raiz, int dado);
+unsigned int busca(char nome[100],char sobrenome[100], Nodo *raiz);
+
+//----------------
+//Estrutura Lista
+//----------------
+typedef struct no{
+    unsigned int number;
+    char nome[100];
+    char sobrenome[100];
+    struct no *prox;
+}tNo;
+typedef struct lista{
+    tNo *cabecalho;
+    int n_elem;
+}tLista;
+
+void inicializaLista(tLista *lista);
+int listaVazia(tLista lista);
+void mostraLista(tLista lista);
+void inserirNoInicio(tLista *lista, int number, char nome[100],char sobrenome[100]);
+unsigned int buscarNomeSobrenome(tLista lista, char nome[100],char sobrenome[100]);
+//----------------
